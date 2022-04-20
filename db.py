d8ee8bd7e926853bb0bf87bf9c757138143a3ae9
Eduardo Margulis <edumarg@gmx.com>
2022-04-20 10:53:48 +0300

    started flask configuration and connection to db

--- db.py
+++ db.py
@@ -1,7 +1,26 @@
 import pymongo
 
 
-client = pymongo.MongoClient(host="mongodb+srv://developer:{password}@cluster0.juo1t.mongodb.net/NLP?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE".format(password="devo1234"), connect=False)
+def connect_to_db():
+    client = pymongo.MongoClient \
+        (host="mongodb+srv://developer:{password}@cluster0.juo1t.mongodb.net/NLP?retryWrites=true&w=majority&ssl=true"
+              "&ssl_cert_reqs=CERT_NONE".format(password="devo1234"), connect=False)
+    return client
 
-print(list(client['NLP']['TERMS'].find({})))
 
+def get_all_data(client):
+    data_dict = {}
+    data = client['NLP']['TERMS'].find({})
+    for term in data:
+        term['_id'] = str(term['_id'])
+        data_dict[term['_id']] = term
+    # print(list(data))
+    return data_dict
+
+
+def add_new_terms(term, client):
+    return client['NLP']['TERMS'].insert_one(term)
+
+
+def get_terms_count(type, client):
+    pass
--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+from flask import Flask, json, request
+from flask_cors import CORS
+import db
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+cors = CORS(app)
+
+my_client = db.connect_to_db()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# GET all data
+@app.route("/home", methods=["GET"])
+def get_all_data():
+    data = db.get_all_data(my_client)
+    response = app.response_class(response=json.dumps(data), status=200, mimetype="application/json")
+    return response
 
 
-# Press the green button in the gutter to run the script.
+# POST a new type
+@app.route("/type/new", methods=["POST"])
+def add_new_terms():
+    terms = request.json
+    print("---received terms--")
+    print(terms)
+    db.add_new_terms(terms, my_client)
+    response = app.response_class(response="Terms added to the database", status=200, mimetype='application/json')
+    return response
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run()
